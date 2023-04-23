b1c572ecf7a6334816bb7c8e5788f8240339c772
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-04-23 19:15:36 +0430

    add first page

---
+++ src/js/main.js
@@ -0,0 +1,105 @@
+import skrollr from "skrollr";
+import Typewriter from 'typewriter-effect/dist/core';
+let s = skrollr.init();
+const $ = document;
+// magnetic button
+const lerp = (current, target, factor) =>
+  current * (1 - factor) + target * factor;
+
+let mousePosition = { x: 0, y: 0 };
+window.addEventListener('mousemove', (e) => {
+  mousePosition.x = e.pageX;
+  mousePosition.y = e.pageY;
+});
+
+const calculateDistance = (x1, y1, x2, y2) => {
+  return Math.hypot(x1 - x2, y1 - y2);
+};
+
+class MagneticObject {
+    constructor(domElement) {
+      this.domElement = domElement;
+      this.boundingClientRect = this.domElement.getBoundingClientRect();
+      this.triggerArea = 200;
+      this.interpolationFactor = 0.8;
+  
+      this.lerpingData = {
+        x: { current: 0, target: 0 },
+        y: { current: 0, target: 0 },
+      };
+  
+      this.render();
+      this.resize();
+    }
+  
+    resize() {
+      window.addEventListener('resize', () => {
+        this.boundingClientRect = this.domElement.getBoundingClientRect();
+      });
+    }
+  
+    render() {
+      const distanceFromMouseToCenter = calculateDistance(
+        mousePosition.x,
+        mousePosition.y,
+        this.boundingClientRect.left + this.boundingClientRect.width / 2,
+        this.boundingClientRect.top + this.boundingClientRect.height / 2
+      );
+  
+      let targetHolder = { x: 0, y: 0 };
+  
+      if (distanceFromMouseToCenter < this.triggerArea) {
+        this.domElement.classList.add('focus');
+        targetHolder.x =
+          (mousePosition.x -
+            (this.boundingClientRect.left + this.boundingClientRect.width / 2)) *
+          0.2;
+        targetHolder.y =
+          (mousePosition.y -
+            (this.boundingClientRect.top + this.boundingClientRect.height / 2)) *
+          0.2;
+      } else {
+        this.domElement.classList.remove('focus');
+      }
+      this.lerpingData['x'].target = targetHolder.x;
+      this.lerpingData['y'].target = targetHolder.y;
+  
+      for (const item in this.lerpingData) {
+        this.lerpingData[item].current = lerp(
+          this.lerpingData[item].current,
+          this.lerpingData[item].target,
+          this.interpolationFactor
+        );
+      }
+  
+      this.domElement.style.transform = `translate(${this.lerpingData['x'].current}px, ${this.lerpingData['y'].current}px)`;
+  
+      window.requestAnimationFrame(() => this.render());
+    }
+  }
+  
+  const button = document.querySelector('#cv_btn');
+  new MagneticObject(button);
+
+//   typing effect
+new Typewriter('#typewriter', {
+    strings: [
+      'Data Specialist',
+      'ML Enthusiastic',
+      'Robots Lover',
+      'A Normal Human Being',
+    ],
+    autoStart: true,
+    loop: true,
+  });
+//   dark and light theme
+  $.getElementById('theme_btn').addEventListener('click', function () {
+    if ($.body.classList.contains('light')) {
+      this.firstElementChild.classList.replace('uil-moon', 'uil-sun');
+    } else {
+      this.firstElementChild.classList.replace('uil-sun', 'uil-moon');
+    }
+    $.body.classList[$.body.classList.contains('light') ? 'remove' : 'add'](
+      'light'
+    );
+  });
